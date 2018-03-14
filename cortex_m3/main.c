66017eb224320419151498adb1400151503b72a1
chmodJack <158754845@qq.com>
2018-03-14 18:28:02 +0800

    20180314

---
+++ cortex_m3/main.c
@@ -0,0 +1,23 @@
+/*************************************************************************
+    > File Name       : main.c
+    > Author          : chmodJack
+    > Mail            : 158754845@qq.com
+    > GitHub          : https://github.com/chmodJack 
+    > Created Time    : 2018年03月14日 星期三 11时19分53秒
+    > Description     : 
+*************************************************************************/
+
+/*
+int init(int a,int b,int c,int d)
+{
+	d=a+b+c+d;
+	return 0;
+}
+*/
+
+unsigned char i=0;
+int main(int num,const char* str)
+{
+	i++;
+    return 0;
+}
---
+++ cortex_m3/start.s
@@ -0,0 +1,89 @@
+.global _stext
+.section .vectors,"a"
+.align 2
+_stext:
+	.word 0xffff0000 @stack pointer
+	.word reset
+	.word nmi
+	.word hard_fault
+	.word mem_manage
+	.word bus_fault
+	.word usage_fault
+	.word 0
+	.word 0
+	.word 0
+	.word 0
+	.word svc
+	.word debug_mon
+	.word 0
+	.word pend_sv
+	.word sys_tick
+	@continue external exceptions...
+
+.global reset
+.section .text
+.thumb_func
+.align 2
+reset:
+	mov r0,#1
+	mov r1,#2
+	mov r2,#3
+	mov r3,#4
+
+	dmb
+	dsb
+	isb
+
+	bl init
+	cmp r0,#0
+	bne .
+
+	mov r0,#1
+	ldr r1,=str
+	bl main
+	cmp r0,#0
+	bne .
+	b reset
+
+.section .rodata
+.align 2
+str:
+	.ascii "Hello world!\0"
+
+.global init
+.section .text
+.thumb_func
+.weak init
+.align 2
+init:
+	mov r0,#0
+	mov pc,lr
+
+.weak nmi
+.weak hard_fault
+.weak mem_manage
+.weak bus_fault
+.weak usage_fault
+.weak svc
+.weak debug_mon
+.weak pend_sv
+.weak sys_tick
+
+nmi:
+	b .
+hard_fault:
+	b .
+mem_manage:
+	b .
+bus_fault:
+	b .
+usage_fault:
+	b .
+svc:
+	b .
+debug_mon:
+	b .
+pend_sv:
+	b .
+sys_tick:
+	b .
